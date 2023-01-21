3cf546751d714268e8811c6735f79eb7279fffb4
alziputra <alziputra12@gmail.com>
2023-01-21 19:46:22 +0700

    master : buat form login untuk admin

---
+++ application/views/admin/dashboard.php
@@ -0,0 +1,90 @@
+<div class="container p-md-5 ">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card shadow p-30">
+                <div class="media">
+                    <div class="media-body media-text-right">
+                        <h2><?php echo $countUser; ?></h2>
+                        <p class="m-b-0">User/Konsumen</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-3">
+            <div class="card shadow p-30">
+                <div class="media">
+                    <div class="media-body media-text-right">
+                        <h2><?php echo $countStore; ?></h2>
+                        <p class="m-b-0">Restoran</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card shadow p-30">
+                <div class="media">
+                    <div class="media-body media-text-right">
+                        <h2><?php echo $countDish; ?></h2>
+                        <p class="m-b-0">Menu</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="col-md-3">
+            <div class="card shadow p-30">
+                <div class="media">
+                    <div class="media-body media-text-right">
+                        <h2><?php echo $countOrders; ?></h2>
+                        <p class="m-b-0">Total Order</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card shadow p-30">
+                <div class="media">
+                    <div class="media-body media-text-right">
+                        <h2><?php echo $countCategory;?></h2>
+                        <p class="m-b-0">Kategori</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card shadow p-30">
+                <div class="media">
+                    <div class="media-body media-text-right">
+                        <h2><?php echo $countPendingOrders; ?></h2>
+                        <p class="m-b-0">Pesanan ditunda</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card shadow p-30">
+                <div class="media">
+                    <div class="media-body media-text-right">
+                        <h2><?php echo $countDeliveredOrders; ?></h2> 
+                        <p class="m-b-0">Pesanan terkirim</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-3">
+            <div class="card shadow p-30">
+                <div class="media">
+                    <div class="media-body media-text-right">
+                        <h2><?php echo $countRejectedOrders; ?></h2>
+                        <p class="m-b-0">Pesanan dibatalkan</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
